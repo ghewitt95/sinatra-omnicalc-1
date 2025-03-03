179b4e1da2454e61058f52f02559cd57e639851f
Gabriel Hewitt <97916103+ghewitt95@users.noreply.github.com>
2025-03-03 17:03:35 +0000

    updated square root calculator

--- app.rb
+++ app.rb
@@ -12,9 +12,25 @@ get("/square/new") do
   erb(:new_square_calc)
 end
 
+get("/square/results") do
+  @the_num = params.fetch("user_number").to_f
+
+  @the_result = @the_num ** 2
+
+  erb(:square_results)
+end
+
 get("/square_root/new") do
 
   erb(:new_root_calc)
+end
+
+get("/square_root/result") do
+  @the_num = params.fetch("user_number").to_f
+
+  @the_result = Math.sqrt(@the_num).to_i
+
+  erb(:root_results)
 end
 
 get("/payment/new") do
